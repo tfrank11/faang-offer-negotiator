d968a2acfd1580fbe87a6775810376b35bccf30c
tfrank11 <77943795+tfrank11@users.noreply.github.com>
2024-03-23 12:48:31 -0400

    feat: redirect back to landing upon signout

--- src/common/useThreadId.ts
+++ src/common/useThreadId.ts
@@ -1,18 +1,21 @@
 import { useNavigate, useParams } from "react-router-dom";
 import { useAuth } from "./useAuth";
-import { useEffect } from "react";
+import { useEffect, useRef } from "react";
 
 export const useThreadId = () => {
   const auth = useAuth();
   const navigate = useNavigate();
   const { threadId } = useParams();
+  const isLoggedIn = useRef(false);
 
-  // TODO: fix
   useEffect(() => {
-    // this always redirects for some reason
-    if (!auth.user) {
-      // navigate("/")
-    }
+    // The auth.user.uid values comes in on a slight lag, so this is to avoid errant redirects
+    isLoggedIn.current = !!auth.user?.uid;
+    setTimeout(() => {
+      if (!isLoggedIn.current) {
+        navigate("/");
+      }
+    }, 500);
   }, [auth.user, navigate, threadId]);
 
   return {
