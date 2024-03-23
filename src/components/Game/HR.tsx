5dd311edd3e13271029f5334901b2a27d14a47b0
tfrank11 <77943795+tfrank11@users.noreply.github.com>
2024-03-23 12:28:00 -0400

    fix: image paths

--- src/components/Game/HR.tsx
+++ src/components/Game/HR.tsx
@@ -9,19 +9,19 @@ const HR: React.FC<Props> = ({ mood }) => {
   const imgSrc = useMemo(() => {
     switch (mood) {
       case HRMood.NORMAL_1: {
-        return "normal1.png";
+        return "/normal1.png";
       }
       case HRMood.NORMAL_2: {
-        return "normal2.png";
+        return "/normal2.png";
       }
       case HRMood.NORMAL_3: {
-        return "normal3.png";
+        return "/normal3.png";
       }
       case HRMood.OFFER: {
-        return "offer.png";
+        return "/offer.png";
       }
       case HRMood.RESCINDED: {
-        return "upset.png";
+        return "/upset.png";
       }
     }
   }, [mood]);
